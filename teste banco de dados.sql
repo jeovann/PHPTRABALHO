8951f4231e5fbe800c63f2b9abafd30c5cd0eaad
Jeovan <jeovann@hotmail.com>
2018-06-29 21:32:25 -0400

    Trabalho de phProfWillian

---
+++ teste banco de dados.sql
@@ -0,0 +1,7 @@
+USE biblioteca;
+
+INSERT INTO livros(id,cod_livro,nome_livro,des_livro)
+	VALUES(null,'1818','JAVA SOLID',
+           'JAVA PADRÕES DE PROJETO');
+           
+SELECT * FROM llivrosivros;
